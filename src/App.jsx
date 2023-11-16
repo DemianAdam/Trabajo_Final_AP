cfe3426d06562f79af38ffd21643006e31757651
satoshiwolf <wolfensonsatoshi@gmail.com>
2023-11-16 00:00:14 -0300

    add reference to lists and tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,61 @@
 import TaskComponent from "./components/task.component"
 function App() {
 
-  //un ejmplo de uso de la tarea
+  //un ejmplo de conjunto de listas y notas
+
+  const tasksList = [
+    {
+      id: "0",
+      title: "pruebas",
+      tasks: [
+        {
+          id: 0,
+          title: 'momo',
+          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
+          label: '#F8BD1C'
+        },
+        {
+          id: 1,
+          title: 'momo',
+          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
+          label: '#F8BD1C'
+        }
+      ]
+    },
+    {
+      id: "1",
+      title: "finalizado",
+      tasks: [
+        {
+          id: 0,
+          title: 'momo',
+          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
+          label: '#F8BD1C'
+        },
+        {
+          id: 1,
+          title: 'momo',
+          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
+          label: '#F8BD1C'
+        }
+      ]
+    }
+  ]
+
+  //un ejmplo de uso de la nota
   const taskData = {
+    id: 0,
     title: 'momo',
     body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
     label: '#F8BD1C'
-}
+  }
+
+
+
 
   return (
     <>
-      <TaskComponent taskData={taskData}/>
+      <TaskComponent taskData={taskData} />
     </>
   )
 }
