3e5c050417a2f3ad3258852fa45dc162b8b128fb
satoshiwolf <wolfensonsatoshi@gmail.com>
2023-11-15 23:39:02 -0300

    add taskcomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import TaskComponent from "./components/task.component"
 function App() {
+
+  //un ejmplo de uso de la tarea
+  const taskData = {
+    title: 'momo',
+    body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
+    label: '#F8BD1C'
+}
+
   return (
     <>
+      <TaskComponent taskData={taskData}/>
     </>
   )
 }
---
+++ src/components/task.component.jsx
@@ -0,0 +1,24 @@
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+
+
+export default function TaskComponent(props) {
+    const [taskData, settaskData] = useState(props.taskData)
+
+    return (
+        <div className='bg-[#18191B80]/50  border-white/20 w-[22.625rem] max-h-72 text-white border rounded-lg'>
+            <CardContent>
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="8" viewBox="0 0 60 8" fill="none">
+                    <rect width="60" height="8" rx="4" fill={taskData.label} />
+                </svg>
+                <Typography variant="h5" className='pb-2'>
+                    {taskData.title}
+                </Typography>
+                <Typography>
+                    {taskData.body}
+                </Typography>
+            </CardContent>
+        </div>
+    )
+}
