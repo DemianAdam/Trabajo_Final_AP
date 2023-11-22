bf5c123910f9a7c8a136d0d3e14869d25c10bb33
NahuelEF <nahuelfedele@gmail.com>
2023-11-22 20:27:36 -0300

    add task form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import TaskComponent from "./components/task.component"
-import Categoria from "./components/categoria"
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-function App() {
+import { Button, InputBase, InputLabel, Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Categoria from "./components/categoria";
+import TaskComponent from "./components/task.component";
+import TaskForm from "./components/TaskForm";
 
+function App() {
   //un ejmplo de conjunto de listas y notas
 
   const tasksList = [
@@ -13,17 +15,17 @@ function App() {
       tasks: [
         {
           id: 0,
-          title: 'momo',
-          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
-          label: '#F8BD1C'
+          title: "momo",
+          body: "qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem",
+          label: "#F8BD1C",
         },
         {
           id: 1,
-          title: 'momo',
-          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
-          label: '#F8BD1C'
-        }
-      ]
+          title: "momo",
+          body: "qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem",
+          label: "#F8BD1C",
+        },
+      ],
     },
     {
       id: "1",
@@ -31,30 +33,30 @@ function App() {
       tasks: [
         {
           id: 0,
-          title: 'momo',
-          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
-          label: '#F8BD1C'
+          title: "momo",
+          body: "qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem",
+          label: "#F8BD1C",
         },
         {
           id: 1,
-          title: 'momo',
-          body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
-          label: '#F8BD1C'
-        }
-      ]
-    }
-  ]
+          title: "momo",
+          body: "qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem",
+          label: "#F8BD1C",
+        },
+      ],
+    },
+  ];
 
   //un ejmplo de uso de la nota
   const taskData = {
     id: 0,
-    title: 'momo',
-    body: 'qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem',
-    label: '#F8BD1C'
-  }
+    title: "momo",
+    body: "qui dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem",
+    label: "#F8BD1C",
+  };
 
   return (
-    <div>
+    <>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={1}>
           <Grid item xs={12}>
@@ -69,13 +71,11 @@ function App() {
           <Grid item xs={12}>
             <TaskComponent taskData={taskData} />
           </Grid>
-          
         </Grid>
       </Box>
-
-
-    </div>
-  )
+      <TaskForm />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,73 @@
+import { Box, Button, InputBase, InputLabel, Stack } from "@mui/material";
+
+export default function TaskForm() {
+  return (
+    <Box
+      component="form"
+      onSubmit={(e) => e.preventDefault()}
+      className="flex w-[22.625rem] flex-col items-start gap-5 rounded-lg border border-white/20 bg-[#18191B80]/50 px-6 py-4 text-white"
+    >
+      <Stack direction="column" spacing={1.25} useFlexGap>
+        <InputLabel
+          htmlFor="titulo-task"
+          className="w-[287px] text-[13px] font-light leading-[19px] text-inherit"
+        >
+          Titulo
+        </InputLabel>
+        <InputBase
+          type="text"
+          id="titulo-task"
+          placeholder="Lorem ipsum dolor sit"
+          fullWidth
+          slotProps={{ input: { className: "py-1.5" } }}
+          className="h-[30px] rounded-[5px] border border-white border-opacity-20 bg-zinc-900 bg-opacity-50 px-3 text-[13px] font-light leading-[19px] text-inherit"
+        />
+      </Stack>
+      <Stack direction="column" spacing={1.25} useFlexGap>
+        <InputLabel
+          htmlFor="cuerpo-task"
+          className="w-[287px] text-[13px] font-light leading-[19px] text-inherit"
+        >
+          Cuerpo
+        </InputLabel>
+        <InputBase
+          type="text"
+          id="cuerpo-task"
+          multiline
+          minRows={3}
+          fullWidth
+          placeholder="Lorem ipsum dolor sit amet consectetur, adipiscing elit auctor penatibus volutpat posuere, sodales praesent libero mus"
+          slotProps={{ input: { className: "py-1.5" } }}
+          className="rounded-[5px] border border-white border-opacity-20 bg-zinc-900 bg-opacity-50 px-3  text-[13px] font-light leading-[19px] text-inherit"
+        />
+      </Stack>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="60"
+        height="8"
+        viewBox="0 0 60 8"
+        fill="none"
+      >
+        <rect width="60" height="8" rx="4" fill="#FF3838" />
+      </svg>
+      <Stack direction="row" spacing={1.5} useFlexGap>
+        <Button
+          variant="contained"
+          disableElevation
+          size="small"
+          className="rounded-[5px] bg-blue-600"
+        >
+          Añadir
+        </Button>
+        <Button
+          variant="outlined"
+          color="error"
+          size="small"
+          className="rounded-[5px]"
+        >
+          Cancelar
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
